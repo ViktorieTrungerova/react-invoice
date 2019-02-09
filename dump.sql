f156d3e4f03fe52aafd24ec3f5833cc4fbdc30fb
Ondrej Krajcik <ondrakrajcik@gmail.com>
2019-02-10 02:30:08 +1300

    update db dump

--- dump.sql
+++ dump.sql
@@ -5,6 +5,33 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP TABLE IF EXISTS `client`;
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `last_name` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_czech_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `client` (`id`, `first_name`, `last_name`, `phone`, `email`) VALUES
+(1,	'Jan',	'Novák',	'+420 123 456 789',	'jan@novak.cz'),
+(2,	'Karel',	'Svoboda',	'+420 333 666 999',	'karel@svoboda.cz'),
+(3,	'František',	'Nový',	'+420 555 888 777',	'frantisek@novy.cz');
+
+DROP TABLE IF EXISTS `invoice`;
+CREATE TABLE `invoice` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_client` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_client` (`id_client`),
+  CONSTRAINT `invoice_ibfk_1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `invoice` (`id`, `id_client`) VALUES
+(1,	1);
+
 DROP TABLE IF EXISTS `invoice_item`;
 CREATE TABLE `invoice_item` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -21,4 +48,4 @@ CREATE TABLE `invoice_item` (
 INSERT INTO `invoice_item` (`id`, `invoice_id`, `name`, `price_without_tax`, `price_with_tax`, `tax_percent`) VALUES
 (2,	1,	'1',	100,	121,	21);
 
--- 2019-02-09 13:26:42
+-- 2019-02-09 13:29:52
